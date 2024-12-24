424032de52cc80c91fc521c07fc49247623a8e9d
magicbowen <e.bowen.wang@icloud.com>
2024-12-24 22:39:09 +0800

    first commit

---
+++ resource/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn compute_cost() -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = compute_cost();
+        assert_eq!(result, 0);
+    }
+}
